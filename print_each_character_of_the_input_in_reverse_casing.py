dc1ad3b449b15419e81e0a4813ae92e0c1f75fff
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 13:39:56 -0800

    print the reversed casing name

--- print_each_character_of_the_input_in_reverse_casing.py
+++ print_each_character_of_the_input_in_reverse_casing.py
@@ -2,3 +2,4 @@
 full_name = input("enter your full name in incorrect casing: ")
 #reverse the casing of each character
 reversed_casing_name = ''.join(char.swapcase() for char in full_name)
+print(reversed_casing_name) #print the reversed casing name
