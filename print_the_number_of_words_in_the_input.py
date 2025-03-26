381cd5ffc1c29468a383be050d88bcee130b3da9
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 13:52:05 -0800

    count and print the number of word

--- print_the_number_of_words_in_the_input.py
+++ print_the_number_of_words_in_the_input.py
@@ -1,3 +1,6 @@
 #ask the user to input a complete statement
 statement = input("Please enter a complete statement: ")
 words = statement.split() #split the statement into words
+#count the number of words
+word_count = len(words)
+print(word_count) #print the number of word
