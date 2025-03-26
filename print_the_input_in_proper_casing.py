2583e6365cbbeadfb988ebe7d5d1e3656654a147
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 13:31:51 -0800

    convert full name in proper casing

--- print_the_input_in_proper_casing.py
+++ print_the_input_in_proper_casing.py
@@ -1,2 +1,4 @@
 #ask the user to input their full name in incorrect casing
 full_name = input("enter your full name in incorrect order: ")
+#convert full name in proper casing
+proper_cased_name = full_name.title()
