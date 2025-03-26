5ae6feaea019b9a826b84fe091aa145c294d8b8d
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 15:37:35 -0800

    convert the input into pascal case

--- print_the_input_in_pascal_case.py
+++ print_the_input_in_pascal_case.py
@@ -1,4 +1,5 @@
 #ask user to input their full name
 full_name = input("enter your full name: ")
 #convert the input into pascal case
+pascal_case_name = to_pascal_case(full_name)
 #print the full name into pascal case
