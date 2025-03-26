f023fa9b527befa36d48d202e98fd651f675cc51
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 15:30:57 -0800

    print the number characters

--- print_the_number_of_characters_in_the_input.py
+++ print_the_number_of_characters_in_the_input.py
@@ -1,3 +1,5 @@
 #ask the user to input their full name
 full_name = input("enter your full name: ")
 num_characters = len(full_name) #compute the numbers of the characters in the input
+#print the number characters
+print(num_characters)
