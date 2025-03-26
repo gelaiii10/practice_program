5b52d75d44aa19f455c8809215f53d47c6884d22
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 13:03:11 -0800

    remove leading spaces using the strip() method

--- ask_user_to_input_their_fullname_with_several_space_characters_at_the_beginning.py
+++ ask_user_to_input_their_fullname_with_several_space_characters_at_the_beginning.py
@@ -1,4 +1,5 @@
 #ask the user to input their fullname with several space
 fullname = input("enter your full name: ")
 #remove leading spaces using the strip() method
+cleaned_fullname = fullname.lstrip()
 #print the cleaned full name
