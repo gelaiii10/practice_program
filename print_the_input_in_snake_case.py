4bf15a154e16e45b3c4c7b31156861aaf0660500
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 15:56:03 -0800

    convert input into snake case

--- print_the_input_in_snake_case.py
+++ print_the_input_in_snake_case.py
@@ -1,1 +1,3 @@
 full_name = input("enter your full name: ") #ask user to input their full name
+#convert the input to snake_case
+snake_case_name = '_'.join(word.lower() for word in full_name.split())
