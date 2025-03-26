364aef9fe3da8139ad340cf5a128b07791d849c7
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 13:09:36 -0800

    added condition and format the number

--- print_the_number_in_6_digit_format._add_zeros_at_the_beginning_to_complete_the_6_digit.py
+++ print_the_number_in_6_digit_format._add_zeros_at_the_beginning_to_complete_the_6_digit.py
@@ -1,2 +1,6 @@
 #ask the user to input a number between 0 and 1000
 num = int(input("enter a number between 0 and 1000: "))
+#check if the number is within the valid range
+if 0 <= num <= 1000:
+    #format the number to 6 digits with leading zeroes
+    formatted_number = f"{num:06}"
