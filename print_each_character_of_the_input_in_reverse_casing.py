3badf47b90a3d381ff3439833f04b71bed99633a
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 13:38:25 -0800

    reverse the casing of each character

--- print_each_character_of_the_input_in_reverse_casing.py
+++ print_each_character_of_the_input_in_reverse_casing.py
@@ -1,2 +1,4 @@
 #ask the user to input their full name in incorrect casing
 full_name = input("enter your full name in incorrect casing: ")
+#reverse the casing of each character
+reversed_casing_name = ''.join(char.swapcase() for char in full_name)
